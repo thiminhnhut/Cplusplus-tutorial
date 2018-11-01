c0d3e518f4288f6a5db9abe1d0e38897896ed1ca
thiminhnhut <thiminhnhut@gmail.com>
2018-11-01 10:50:56 +0700

    String to Byte Arrays

---
+++ tips/String2ByteArrays.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main() {
+    string str = "abcdef";
+
+    unsigned int _size = str.length();
+    cout << "Size = " << _size << endl;
+
+    char* cstr = new char[_size + 1];
+    strcpy(cstr, str.c_str());
+    for (unsigned int i = 0; i < _size; i++) {
+        cout << *(cstr + i) << " ";
+    }
+
+    return 0;
+}
